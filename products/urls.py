4905a8512ce73be3b8251541597a7434b04570b1
Serhii Lakhman <serhii.lakhman.ki.2020@lpnu.ua>
2023-11-30 02:31:29 +0200

    Added login and registration functionality

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-from .views import product_search
+from .views import *
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('new/', views.new), 
     path('search/', product_search, name='product_search'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
     # Add other URL patterns as needed
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import *
 from .models import Product
 
 
@@ -18,3 +24,23 @@ def product_search(request):
     query = request.GET.get('q', '')
     products = Product.objects.filter(name__icontains=query)
     return render(request, 'products_search.html', {'products': products, 'query': query})
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
